b29aa22bac19df06595b0719f208e46db8cc8d6e
Alisson Morais <alissonfpmorais@gmail.com>
2021-10-28 11:53:09 -0300

    feat: update boilerplate features

---
+++ README.md
@@ -0,0 +1,12 @@
+#TS Lib Boilerplate
+
+Repo used as template for new Typescript libraries.
+
+Features:
+- Git pre-commit hook;
+- Git commit linter;
+- Linter (and some pre-configured rules);
+- Prettier;
+- Jest.
+
+
--- test/library.test.ts
+++ test/library.test.ts
@@ -2,7 +2,7 @@ import { Library } from '../src';
 
 describe('Testing Library', () => {
   it('Should sum function sum two given numbers', () => {
-    const library = new Library();
+    const library: Library = new Library();
     expect(library.sum(2, 3)).toEqual(5);
   });
 });
